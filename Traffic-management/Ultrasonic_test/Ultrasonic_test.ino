97d7030192efe600566a1f3f0e6b191c449d7e63
RUPERT CLAISSE - STUDENT <g00423530@atu.ie>
2024-02-09 12:48:47 +0000

    Added scripts to test file to test out ultrasonic sensor

--- Traffic-management/Ultrasonic_test/Ultrasonic_test.ino
+++ Traffic-management/Ultrasonic_test/Ultrasonic_test.ino
@@ -5,6 +5,41 @@ const int ECHO_PIN = 10;
 long duration;
 float cms, inches, distance;
 
+void vehicle_red() {
+  // Turn vehicle traffic lights red
+
+  digitalWrite(VEHICLE_GREEN_TOP, LOW);
+  digitalWrite(VEHICLE_GREEN_BOTTOM, LOW);
+  digitalWrite(VEHICLE_AMBER_TOP, HIGH);
+  digitalWrite(VEHICLE_AMBER_BOTTOM, HIGH);
+  delay(2000);
+  digitalWrite(VEHICLE_AMBER_TOP, LOW);
+  digitalWrite(VEHICLE_AMBER_BOTTOM, LOW);
+  digitalWrite(VEHICLE_RED_TOP, HIGH);
+  digitalWrite(VEHICLE_RED_BOTTOM, HIGH);
+}
+
+void vehicle_green() {
+  digitalWrite(VEHICLE_RED_TOP, LOW);
+  digitalWrite(VEHICLE_RED_BOTTOM, LOW);
+  digitalWrite(VEHICLE_GREEN_TOP, HIGH);
+  digitalWrite(VEHICLE_GREEN_BOTTOM, HIGH);
+}
+
+void ped_red() {
+  digitalWrite(PED_GREEN_RIGHT, LOW);
+  digitalWrite(PED_GREEN_LEFT, LOW);
+  digitalWrite(PED_RED_RIGHT, HIGH);
+  digitalWrite(PED_RED_LEFT, HIGH);
+}
+
+void ped_green() {
+  digitalWrite(PED_RED_RIGHT, LOW);
+  digitalWrite(PED_RED_LEFT, LOW);
+  digitalWrite(PED_GREEN_RIGHT, HIGH);
+  digitalWrite(PED_GREEN_LEFT, HIGH);
+}
+
 void setup() {
   // Serial port begin
   Serial.begin(115200);
@@ -25,14 +60,17 @@ void loop() {
 
   duration = pulseIn(ECHO_PIN, HIGH);
   cms = (duration / 2) / 29.1;
-  distance = 5; // Reconfigure distance variable to edit the 
+  distance = 5; // Reconfigure distance variable if required
   Serial.print("Nearest vehicle at ");
   Serial.print(cms);
   Serial.print(" cm");
 
   // if statement placeholder for interrupt
   if (cms < distance) {
+    Serial.print("Vehicle in range");
     vehicle_green();
+    delay(2000);
     ped_red();
+    delay(5000);
   }
 }
