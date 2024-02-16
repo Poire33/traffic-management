b35da4e9353f7d3d9384c51b407fd531b027be8f
RUPERT CLAISSE - STUDENT <g00423530@atu.ie>
2024-02-16 12:19:29 +0000

    Added header file to Ultrasonic_test as well as 'fixing' its main ino file

--- Traffic-management/Ultrasonic_test/Ultrasonic_test.ino
+++ Traffic-management/Ultrasonic_test/Ultrasonic_test.ino
@@ -1,44 +1,11 @@
 // Prototype code for the ultrasonic sensor to detect a vehicle
 
+#include "scripts.h"
+
 const int TRIG_PIN = 5;
-const int ECHO_PIN = 10;
+const int ECHO_PIN = 18;
 long duration;
 float cms, inches, distance;
-
-void vehicle_red() {
-  // Turn vehicle traffic lights red
-
-  digitalWrite(VEHICLE_GREEN_TOP, LOW);
-  digitalWrite(VEHICLE_GREEN_BOTTOM, LOW);
-  digitalWrite(VEHICLE_AMBER_TOP, HIGH);
-  digitalWrite(VEHICLE_AMBER_BOTTOM, HIGH);
-  delay(2000);
-  digitalWrite(VEHICLE_AMBER_TOP, LOW);
-  digitalWrite(VEHICLE_AMBER_BOTTOM, LOW);
-  digitalWrite(VEHICLE_RED_TOP, HIGH);
-  digitalWrite(VEHICLE_RED_BOTTOM, HIGH);
-}
-
-void vehicle_green() {
-  digitalWrite(VEHICLE_RED_TOP, LOW);
-  digitalWrite(VEHICLE_RED_BOTTOM, LOW);
-  digitalWrite(VEHICLE_GREEN_TOP, HIGH);
-  digitalWrite(VEHICLE_GREEN_BOTTOM, HIGH);
-}
-
-void ped_red() {
-  digitalWrite(PED_GREEN_RIGHT, LOW);
-  digitalWrite(PED_GREEN_LEFT, LOW);
-  digitalWrite(PED_RED_RIGHT, HIGH);
-  digitalWrite(PED_RED_LEFT, HIGH);
-}
-
-void ped_green() {
-  digitalWrite(PED_RED_RIGHT, LOW);
-  digitalWrite(PED_RED_LEFT, LOW);
-  digitalWrite(PED_GREEN_RIGHT, HIGH);
-  digitalWrite(PED_GREEN_LEFT, HIGH);
-}
 
 void setup() {
   // Serial port begin
@@ -51,7 +18,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
+  ped_green();
+  // vehicle_red();
+
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
   digitalWrite(TRIG_PIN, HIGH);
@@ -63,12 +33,12 @@ void loop() {
   distance = 5; // Reconfigure distance variable if required
   Serial.print("Nearest vehicle at ");
   Serial.print(cms);
-  Serial.print(" cm");
+  Serial.print(" cm\n");
   delay(500);
 
   // if statement placeholder for interrupt
   if (cms < distance) {
-    Serial.print("Vehicle in range");
+    Serial.print("Vehicle in range\n");
     vehicle_green();
     delay(2000);
     ped_red();
---
+++ Traffic-management/Ultrasonic_test/scripts.h
@@ -0,0 +1,104 @@
+// This is the header file for all repeatable and pre-defined patterns in the traffic management system.
+
+// Set pins for traffic lights
+#define VEHICLE_RED_TOP 32
+#define VEHICLE_AMBER_TOP 33
+#define VEHICLE_GREEN_TOP 25
+#define VEHICLE_RED_BOTTOM 26
+#define VEHICLE_AMBER_BOTTOM 14
+#define VEHICLE_GREEN_BOTTOM 13
+
+// Set pins for pedestrian beg buttons and lights
+#define PED_RED_RIGHT 0
+#define PED_GREEN_RIGHT 2
+#define BUTTON_RIGHT 15
+#define BUTTON_LED_RIGHT 4
+#define PED_RED_LEFT 23
+#define PED_GREEN_LEFT 22
+#define BUTTON_LEFT 21
+#define BUTTON_LED_LEFT 19
+
+// Set pins for sensors, see slide 20+ for code
+#define TRIG 12
+#define ECHO 35
+
+void vehicle_red() {
+  // Turn vehicle traffic lights red
+
+  digitalWrite(VEHICLE_GREEN_TOP, LOW);
+  digitalWrite(VEHICLE_GREEN_BOTTOM, LOW);
+  digitalWrite(VEHICLE_AMBER_TOP, HIGH);
+  digitalWrite(VEHICLE_AMBER_BOTTOM, HIGH);
+  delay(2000);
+  digitalWrite(VEHICLE_AMBER_TOP, LOW);
+  digitalWrite(VEHICLE_AMBER_BOTTOM, LOW);
+  digitalWrite(VEHICLE_RED_TOP, HIGH);
+  digitalWrite(VEHICLE_RED_BOTTOM, HIGH);
+}
+
+void vehicle_green() {
+  digitalWrite(VEHICLE_RED_TOP, LOW);
+  digitalWrite(VEHICLE_RED_BOTTOM, LOW);
+  digitalWrite(VEHICLE_GREEN_TOP, HIGH);
+  digitalWrite(VEHICLE_GREEN_BOTTOM, HIGH);
+}
+
+void ped_red() {
+  digitalWrite(PED_GREEN_RIGHT, LOW);
+  digitalWrite(PED_GREEN_LEFT, LOW);
+  digitalWrite(PED_RED_RIGHT, HIGH);
+  digitalWrite(PED_RED_LEFT, HIGH);
+}
+
+void ped_green() {
+  digitalWrite(PED_RED_RIGHT, LOW);
+  digitalWrite(PED_RED_LEFT, LOW);
+  digitalWrite(PED_GREEN_RIGHT, HIGH);
+  digitalWrite(PED_GREEN_LEFT, HIGH);
+}
+
+void beg_button() {
+  Serial.println("Beg button function");
+  digitalWrite(BUTTON_LED_RIGHT, HIGH);
+  digitalWrite(BUTTON_LED_LEFT, HIGH);
+  // Insert delay here when using HC-SR04 to detect cyclists and motor vehicles; in any case, limit delay to about 10s
+  vehicle_red();
+  delay(2000);
+  digitalWrite(BUTTON_LED_RIGHT, LOW);
+  digitalWrite(BUTTON_LED_LEFT, LOW);
+  ped_green();
+  delay(17000);
+  ped_red();
+  delay(2000);
+  vehicle_green();
+}
+
+// Respond to presses of the beg button; have right and left hand sides different if applicable (mainly if there is a refuge island)
+void IRAM_ATTR right_beg() {
+  /* beg_button();
+  digitalWrite(VEHICLE_GREEN_TOP, HIGH);
+  digitalWrite(VEHICLE_GREEN_BOTTOM, HIGH);
+  digitalWrite(VEHICLE_AMBER_TOP, HIGH);
+  digitalWrite(VEHICLE_AMBER_BOTTOM, HIGH);
+  digitalWrite(VEHICLE_RED_TOP, HIGH);
+  digitalWrite(VEHICLE_RED_BOTTOM, HIGH);
+  digitalWrite(PED_RED_RIGHT, HIGH);
+  digitalWrite(PED_RED_LEFT, HIGH);
+  digitalWrite(PED_GREEN_RIGHT, HIGH);
+  digitalWrite(PED_GREEN_LEFT, HIGH);
+  digitalWrite(BUTTON_LED_RIGHT, HIGH);
+  digitalWrite(BUTTON_LED_LEFT, HIGH);
+  delay(3000);
+  */
+}
+
+void IRAM_ATTR left_beg() {
+  beg_button();
+}
+
+void check_vehicles() {
+  vehicle_green();
+  ped_red();
+  delay(10000); // Use interrupts to break out of the delay
+  beg_button(); // Used exclusively to turn the pedestrian side to green for now (comment out line to use beg buttons "properly")
+}
